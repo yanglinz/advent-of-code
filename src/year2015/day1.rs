b288f041a83424eaa4abea731032379ba12fa247
Yanglin Zhao <hi@yanglinzhao.com>
2022-04-27 22:38:05 -0400

    Progress on solution

--- src/year2015/day1.rs
+++ src/year2015/day1.rs
@@ -1,3 +1,12 @@
-pub fn part1() -> String {
-  return "Hello world!".to_string() 
+pub fn part1(input: &str) -> u32 {
+    let mut res = 0;
+    for c in input.chars() {
+        if c == '(' {
+            res = res + 1;
+        } else if c == ')' {
+            res = res - 1;
+        }
+    }
+
+    res
 }
