1c640e3a125f8a86174fab9335359c863002a564
Yanglin Zhao <hi@yanglinzhao.com>
2022-04-28 21:01:42 -0400

    Add day2 example

---
+++ src/year2015/day2.rs
@@ -0,0 +1,20 @@
+pub fn part1(parens: &str) -> i32 {
+}
+
+pub fn part2(parens: &str) -> u32 {
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        // assert_eq!(part1("(())"), 0);
+    }
+
+    #[test]
+    fn test_part2() {
+        // assert_eq!(part2(")"), 1);
+    }
+}
