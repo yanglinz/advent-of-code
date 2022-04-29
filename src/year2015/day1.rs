528aa982e309cc5207b4e7d2733e047bc8d10140
Yanglin Zhao <hi@yanglinzhao.com>
2022-04-28 20:49:16 -0400

    Implement day 1 part 2

--- src/year2015/day1.rs
+++ src/year2015/day1.rs
@@ -11,6 +11,25 @@ pub fn part1(parens: &str) -> i32 {
     res
 }
 
+pub fn part2(parens: &str) -> u32 {
+    let mut res = 0;
+    for (i, c) in parens.chars().enumerate() {
+        if c == '(' {
+            res = res + 1;
+        } else if c == ')' {
+            res = res - 1;
+        }
+
+        if res == -1 {
+            let index = i as u32;
+            return index + 1;
+        }
+    }
+
+    let index = parens.len() as u32;
+    index + 1
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -24,5 +43,11 @@ mod tests {
         assert_eq!(part1("))((((("), 3);
         assert_eq!(part1("())"), -1);
         assert_eq!(part1("))("), -1);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2("()())"), 5);
+        assert_eq!(part2(")"), 1);
     }
 }
