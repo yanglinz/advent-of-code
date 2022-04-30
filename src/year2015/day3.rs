3021d12ba290f60b915ba5f18415b52f10f4474c
Yanglin Zhao <hi@yanglinzhao.com>
2022-04-30 16:29:16 -0400

    Implement part2

--- src/year2015/day3.rs
+++ src/year2015/day3.rs
@@ -27,26 +27,35 @@ fn get_next_position(current: &Position, movement: &char) -> Position {
             y: current.y,
         },
         _ => {
-            panic!("Unexpected char")
+            panic!("Unexpected char: {}", _);
         }
     }
 }
 
 pub fn part1(directions: &str) -> u32 {
     let mut tracker: HashMap<Position, u32> = HashMap::new();
-
     let mut current = Position { x: 0, y: 0 };
     tracker.insert(Position { x: 0, y: 0 }, 1);
-    for c in directions.chars() {
-        current = get_next_position(&current, &c);
+
+    for c in directions.trim().chars() {
         tracker.insert(get_next_position(&current, &c), 1);
+        current = get_next_position(&current, &c);
     }
 
-    tracker.len() as u32
+    tracker.keys().len() as u32
 }
 
 pub fn part2(directions: &str) -> u32 {
-    42
+    let mut tracker: HashMap<Position, u32> = HashMap::new();
+    let mut current = Position { x: 0, y: 0 };
+    tracker.insert(Position { x: 0, y: 0 }, 1);
+
+    for c in directions.trim().chars() {
+        tracker.insert(get_next_position(&current, &c), 1);
+        current = get_next_position(&current, &c);
+    }
+
+    tracker.keys().len() as u32
 }
 
 #[cfg(test)]
@@ -62,6 +71,8 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        // assert_eq!(part2("2x3x4"), 34);
+        assert_eq!(part2(">"), 3);
+        assert_eq!(part2("^>v<"), 3);
+        assert_eq!(part2("^v^v^v^v^v"), 11);
     }
 }
