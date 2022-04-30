c44a56b71a0a9d996b698557c6d0172763651719
Yanglin Zhao <hi@yanglinzhao.com>
2022-04-30 17:07:56 -0400

    Remove this

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs;
 use std::path;
 
@@ -5,7 +6,7 @@ use clap::Parser;
 
 mod year2015;
 
-fn get_input() -> String {
+fn get_input(year: u16, day: u8) -> String {
     let data_file = path::Path::new("./src/year2015/data/day3.txt");
     let content = fs::read_to_string(data_file);
 
@@ -21,19 +22,27 @@ fn get_input() -> String {
 struct Args {
     /// Advent calendar year
     #[clap(short, long)]
-    year: String,
+    year: u16,
 
     /// Advent calendar day
     #[clap(short, long)]
-    day: String,
+    day: u8,
 }
 
 fn main() {
     let args = Args::parse();
 
+    let year_2015_days = HashMap::from([
+        (1, year2015::day1::part1),
+        (2, year2015::day2::part2),
+        (3, year2015::day3::part2),
+    ]);
+
+    // let mapping
+
     println!("{:?}", args);
 
-    let input = get_input();
+    let input = get_input(2015, 1);
     let result = year2015::day3::part2(&input);
     println!("Result: {}", result);
 }
