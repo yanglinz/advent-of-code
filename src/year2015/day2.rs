16eb39a21d9a03b9c4bc772773ee46dd6689ad4a
Yanglin Zhao <hi@yanglinzhao.com>
2022-04-30 21:17:13 -0400

    Fix clippy issues

--- src/year2015/day2.rs
+++ src/year2015/day2.rs
@@ -1,7 +1,7 @@
 pub fn part1(dimensions: &str) -> u32 {
     fn get_area(l: u32, w: u32, h: u32) -> u32 {
         let mut smallest_side = [l, w, h];
-        smallest_side.sort();
+        smallest_side.sort_unstable();
         let slack = smallest_side[0] * smallest_side[1];
 
         2 * l * w + 2 * w * h + 2 * h * l + slack
@@ -9,7 +9,7 @@ pub fn part1(dimensions: &str) -> u32 {
 
     let mut res = 0;
     for d in dimensions.split_whitespace() {
-        let dimensions: Vec<&str> = d.split("x").collect();
+        let dimensions: Vec<&str> = d.split('x').collect();
         let l = dimensions[0].parse().unwrap();
         let w = dimensions[1].parse().unwrap();
         let h = dimensions[2].parse().unwrap();
@@ -22,14 +22,14 @@ pub fn part1(dimensions: &str) -> u32 {
 pub fn part2(dimensions: &str) -> u32 {
     fn get_ribbon_length(l: u32, w: u32, h: u32) -> u32 {
         let mut smallest_side = [l, w, h];
-        smallest_side.sort();
+        smallest_side.sort_unstable();
 
         2 * (smallest_side[0] + smallest_side[1]) + l * w * h
     }
 
     let mut res = 0;
     for d in dimensions.split_whitespace() {
-        let dimensions: Vec<&str> = d.split("x").collect();
+        let dimensions: Vec<&str> = d.split('x').collect();
         let l = dimensions[0].parse().unwrap();
         let w = dimensions[1].parse().unwrap();
         let h = dimensions[2].parse().unwrap();
