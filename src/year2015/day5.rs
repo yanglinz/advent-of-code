5e8788f10950d8838d49b09a4f8973dfeed5a146
Yanglin Zhao <hi@yanglinzhao.com>
2022-05-03 22:09:34 -0400

    Implement day5

--- src/year2015/day5.rs
+++ src/year2015/day5.rs
@@ -1,3 +1,5 @@
+use fancy_regex::Regex;
+
 fn has_three_vowels(s: &str) -> bool {
     let mut num_vowels = 0;
     let vowels = "aeiou";
@@ -49,10 +51,6 @@ fn is_nice(s: &str) -> bool {
     true
 }
 
-fn is_nice_redux(s: &str) -> bool {
-    false
-}
-
 pub fn part1(strings: &str) -> u32 {
     let mut count = 0;
     for s in strings.trim().split("\n") {
@@ -64,8 +62,39 @@ pub fn part1(strings: &str) -> u32 {
     count
 }
 
+fn has_pairs(s: &str) -> bool {
+    let re = Regex::new(r"(\w\w)\w*\1").unwrap();
+    let m = re.is_match(s);
+    if m.is_ok() {
+        m.unwrap()
+    } else {
+        false
+    }
+}
+
+fn has_sandwich(s: &str) -> bool {
+    let re = Regex::new(r"(\w)\w\1").unwrap();
+    let m = re.is_match(s);
+    if m.is_ok() {
+        m.unwrap()
+    } else {
+        false
+    }
+}
+
+fn is_nice_redux(s: &str) -> bool {
+    has_pairs(s) && has_sandwich(s)
+}
+
 pub fn part2(strings: &str) -> u32 {
-    1
+    let mut count = 0;
+    for s in strings.trim().split("\n") {
+        if is_nice_redux(&s) {
+            count += 1
+        }
+    }
+
+    count
 }
 
 #[cfg(test)]
@@ -73,7 +102,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_helpers() {
+    fn test_part1() {
         assert_eq!(has_three_vowels("aio"), true);
         assert_eq!(has_three_vowels("aaa"), true);
         assert_eq!(has_three_vowels("abc"), false);
@@ -95,8 +124,20 @@ mod tests {
     }
 
     #[test]
-    fn test_part1() {}
+    fn test_part2() {
+        assert_eq!(has_pairs("xyxy"), true);
+        assert_eq!(has_pairs("xyx"), false);
+        assert_eq!(has_pairs("aabcdefgaa"), true);
+        assert_eq!(has_pairs("abcdefga"), false);
 
-    #[test]
-    fn test_part2() {}
+        assert_eq!(has_sandwich("xyx"), true);
+        assert_eq!(has_sandwich("abcdefeghi"), true);
+        assert_eq!(has_sandwich("efe"), true);
+        assert_eq!(has_sandwich("abc"), false);
+
+        assert_eq!(is_nice_redux("qjhvhtzxzqqjkmpb"), true);
+        assert_eq!(is_nice_redux("xxyxx"), true);
+        assert_eq!(is_nice_redux("uurcxstgmygtbstg"), false);
+        assert_eq!(is_nice_redux("ieodomkazucvgmuy"), false);
+    }
 }
